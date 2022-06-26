85821ad0cb8a64df28667d584930c96945fc59b7
Samuele Pagnotta <76209569+pgnsamu@users.noreply.github.com>
2022-06-27 00:03:42 +0200

    Update descri.swift

--- bar2/View/descri.swift
+++ bar2/View/descri.swift
@@ -6,73 +6,6 @@
 //
 
 import SwiftUI
-/*
-struct descri: View {
-    @EnvironmentObject var viewModel: AuthenticationViewModel
-    @EnvironmentObject var userurl: UserURLSession
-    var temp: prodotto
-    @State var qnt: Int
-    @State var confermato: Bool = false
-    var body: some View {
-        VStack{
-            //Text(temp.des).bold()
-
-            Stepper(value: $qnt) {
-                Text("Quantità")
-            }
-            HStack{
-                Spacer()
-                Text("\(qnt)")
-            }
-
-            let prezzo: Double = (temp.price as NSString).doubleValue
-            let fin: Double = Double(qnt) * prezzo
-            let prodotto: Cart = Cart(id: temp.id, des: temp.des, price: fin, num: qnt)
-            
-            Text("\(String(format: "%.2f", fin)) €")
-            Spacer()
-            Button(action: {
-                if(prodotto.num != 0){
-                    var a: Int = 0
-                    var ctrl: Bool = true
-                    var nvprd: Cart = Cart(id: prodotto.id, des: prodotto.des, price: prodotto.price, num: prodotto.num)
-                    for i in riep{
-                        if(prodotto.id == i.id ){
-                            if((prodotto.num + i.num)>0){
-                                nvprd = Cart(id: prodotto.id, des: prodotto.des, price: (prodotto.price + i.price), num: (prodotto.num + i.num))
-                                riep.remove(at: a)
-                            }else{
-                                ctrl = false
-                                riep.remove(at: a)
-                            }
-                        }
-                       a += 1
-                    }
-                    if(ctrl){
-                        riep.append(nvprd)
-                    }
-                    
-                }
-                somma = Checkout(riep: riep)
-                confermato = true
-                //userurl.addChart(qta: <#T##Int#>, id: <#T##Int#>)
-            }, label: {
-                Text("Conferma")
-            })
-            NavigationLink(destination: lista()
-                            .navigationBarHidden(true),
-                           isActive: $confermato){EmptyView()}
-                .navigationBarTitle(temp.des, displayMode: .inline)
-        }.padding()
-    }
-}
-*/
-struct descri_Previews: PreviewProvider {
-    static var previews: some View {
-        descriz(temp: array[1], qnt: 1).environmentObject(AuthenticationViewModel())
-            .environmentObject(UserURLSession())
-    }
-}
 
 
 struct descriz: View {
@@ -84,8 +17,6 @@ struct descriz: View {
     @State var confermato: Bool = false
     var body: some View {
         VStack{
-            //Text(temp.des).bold()
-
             Stepper(value: $qnt) {
                 Text("Quantità")
             }
@@ -129,9 +60,7 @@ struct descriz: View {
                     
                 }
                 somma = Checkout(riep: riep)
-                //confermato = true
                 self.mode.wrappedValue.dismiss()
-                //userurl.addChart(qta: <#T##Int#>, id: <#T##Int#>)
             }, label: {
                 Text("Conferma")
             })
@@ -140,5 +69,12 @@ struct descriz: View {
                            isActive: $confermato){EmptyView()}
                 .navigationBarTitle(temp.des, displayMode: .inline)
         }.padding()
+    }
+}
+
+struct descri_Previews: PreviewProvider {
+    static var previews: some View {
+        descriz(temp: array[1], qnt: 1).environmentObject(AuthenticationViewModel())
+            .environmentObject(UserURLSession())
     }
 }
